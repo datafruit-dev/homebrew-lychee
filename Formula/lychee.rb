9ecb39af61502733adaa90ba685b7c405affcf8b
Nicholas Papciak <nicholaspapciak@gmail.com>
2025-10-28 14:14:37 -0700

    Update to v0.2.0

--- Formula/lychee.rb
+++ Formula/lychee.rb
@@ -2,7 +2,7 @@ class Lychee < Formula
   desc "Browser-based interface for Claude Code"
   homepage "https://github.com/datafruit-dev/lychee"
   url "https://github.com/datafruit-dev/lychee/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "e97591f92042dc638484d8ad7750450fd954d7859b1bcda5a967948a8b5d5b91"
+  sha256 "6e34bc87f0b1b593005181c4488140fc8a9fc88b314c856a947801e5dd9ed8e1"
   license "MIT"
   head "https://github.com/datafruit-dev/lychee.git", branch: "main"
 
